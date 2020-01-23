ca14ff3af68d952ad6a71321f793123bdaf72368
Your Name <you@example.com>
2020-01-22 17:02:02 -0800

    end of lab time

---
+++ madlibs.py
@@ -0,0 +1,83 @@
+"""A madlib game that compliments its users."""
+
+from random import choice
+
+from flask import Flask, render_template, request
+
+# "__name__" is a special Python variable for the name of the current module.
+# Flask wants to know this to know what any imported things are relative to.
+app = Flask(__name__)
+
+AWESOMENESS = [
+    'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza',
+    'oh-so-not-meh', 'brilliant', 'ducky', 'coolio', 'incredible', 'wonderful',
+    'smashing', 'lovely',
+]
+
+
+@app.route('/')
+def start_here():
+    """Display homepage."""
+
+    return "Hi! This is the home page."
+
+
+@app.route('/hello')
+def say_hello():
+    """Say hello to user."""
+
+    return render_template("hello.html")
+
+
+@app.route('/greet')
+def greet_person():
+    """Greet user with compliment."""
+
+    player = request.args.get("person")
+
+    compliment = choice(AWESOMENESS)
+
+    return render_template("compliment.html",
+                           person=player,
+                           compliment=compliment)
+
+
+@app.route('/game')
+def show_madlib_form():
+    """Shows madlib form if user selects yes"""
+
+    response = request.args.get("game")
+
+    if response == "yes":
+        return render_template("game.html")
+    else:
+        return render_template("goodbye.html")
+
+@app.route('/madlib')
+def show_madlib():
+
+    color = request.args.get("color")
+    noun = request.args.get("noun")
+    person = request.args.get("person")
+    adjective = request.args.get("adjective")
+    noun_2 = request.args.get("noun_2")
+    run = request.args.get("run")
+    jump = request.args.get("jump")
+    wrap = request.args.get("wrap")
+    sing = request.args.get("sing")
+    # verbs = [run, jump, wrap, sing]
+    # print(verbs)
+
+    return render_template("madlib.html",
+                            color=color,
+                            noun=noun,
+                            person=person,
+                            adjective=adjective,
+                            noun_2 =noun_2,)
+                            # verbs=verb)
+
+if __name__ == '__main__':
+    # Setting debug=True gives us error messages in the browser and also
+    # "reloads" our web app if we change the code.
+
+    app.run(debug=True)
